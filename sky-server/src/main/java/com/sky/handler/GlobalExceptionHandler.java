10b35fc2ddc0c37c7b6a079641e2dcce9a79c8e5
sun <xinsun.cs@gmail>
2024-09-25 23:29:42 +0800

    完成新增员工功能

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,22 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+
+//    java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '绍国秀' for key 'employee.idx_username'
+
+    @ExceptionHandler
+    public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        log.error(message);
+
+        if(message.contains("Duplicate entry")){
+            String username = message.split(" ")[2];
+            return Result.error("username" + username + MessageConstant.ALREADY_EXISTS);
+        } else {
+            return Result.error(message);
+        }
     }
 
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,10 +12,15 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -52,6 +60,42 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        // 拷贝对象属性
+        BeanUtils.copyProperties(employeeDTO, employee);
+
+        // 设置默认密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        // 设置账号状态
+        employee.setStatus(StatusConstant.DISABLE);
+
+        // 设置创建时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 获取当前员工 id
+        Long currentId = BaseContext.getCurrentId();
+
+        // 设置创建人
+        employee.setCreateUser(currentId);
+        employee.setUpdateUser(currentId);
+
+//        log.info("当前员工id: {}", currentId);
+
+        BaseContext.removeCurrentId();
+
+        employeeMapper.insert(employee);
+
     }
 
 }
